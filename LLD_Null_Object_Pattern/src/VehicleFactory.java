57ccbabf81f02072df54610ac08dfd5c10b1b6ec
Niteshkumar152 <nitesh26589@gmail.com>
2024-01-15 16:20:01 +0530

    LLD Null Object handle

---
+++ LLD_Null_Object_Pattern/src/VehicleFactory.java
@@ -0,0 +1,9 @@
+public class VehicleFactory {
+
+    static Vehicle getVehicleObject(String typeOfVehicle){
+        if("Car".equals(typeOfVehicle)){
+            return new Car();
+        }
+        return new NullVehicle();
+    }
+}
